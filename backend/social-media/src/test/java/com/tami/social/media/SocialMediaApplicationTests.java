a3d87276a3bd0ed62fba0c434f0aed31385355dc
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2024-01-16 10:47:40 +0500

    backend init

---
+++ backend/social-media/src/test/java/com/tami/social/media/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tami.social.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
