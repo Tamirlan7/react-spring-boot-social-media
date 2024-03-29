df7091df9b7083a67443a1e3ca34573462f3a212
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2024-03-29 14:55:08 +0600

    api gateway integration

--- backend/post-service/src/main/java/by/tami/post/service/controller/PostController.java
+++ backend/post-service/src/main/java/by/tami/post/service/controller/PostController.java
@@ -2,6 +2,8 @@ package by.tami.post.service.controller;
 
 import by.tami.post.service.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService pService;
+
+    @GetMapping
+    ResponseEntity<String> get() {
+        return ResponseEntity.ok("Test!");
+    }
 
 }
